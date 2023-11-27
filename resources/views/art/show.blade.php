07ad5d0c8f4dafe68dc44f9876c6f17c75fe4477
Amari Low <sceptile_rayquaza@hotmail.com>
2023-11-27 17:58:05 +1000

    Fix user variable leaking out of scope

--- resources/views/art/show.blade.php
+++ resources/views/art/show.blade.php
@@ -4,34 +4,35 @@
 		<h1>{{ $artwork->title }}</h1>
 		
 		{{ sizeof($owner_ids) > 1 ? "Artists:" : "Artist:" }}
-		@foreach($artwork->users()->get() as $i=>$user)
-				<a href="{{ route("user", ["username" => $user->name]) }}">
-					{!! $user->getFlairHTML() !!}
-					{{ $user->name }}</a><!--
+		@foreach($artwork->users()->get() as $i => $owner)
+				<a href="{{ route("user", ["username" => $owner->name]) }}">
+					{!! $owner->getFlairHTML() !!}
+					{{ $owner->name }}</a><!--
 					-->@if(!$loop->last), @endif
 		@endforeach
 		
-		@include("tags.taglist", ["user"=>$user, "tags"=>$artwork->tags])
-		
-		@if(sizeof($folders) > 0)
-		<div>
-			@if(sizeof($folders) > 1)
-			Folders:
-			@else 
-			Folder:
-			@endif
-
-			@foreach($folders as $folder)
-				<a href="{{ route("user", ["username" => $folder->user()->first()->name]) }}">
-					{{$folder->user()->first()->name}}</a>'s
-				<a href="{{ route("folders.show", ["username" => $folder->user()->first()->name, "folder"=>$folder]) }}">
-					{{ $folder->title }}</a><!--
-				-->@if(!$loop->last), @endif
-			@endforeach
-		</div>
-		@endif
 		
 		<div class="art-info">
+			@include("tags.taglist", ["tags"=>$artwork->tags])
+			
+			@if(sizeof($folders) > 0)
+			<div>
+				@if(sizeof($folders) > 1)
+				Folders:
+				@else 
+				Folder:
+				@endif
+
+				@foreach($folders as $folder)
+					<a href="{{ route("user", ["username" => $folder->user()->first()->name]) }}">
+						{{$folder->user()->first()->name}}</a>'s
+					<a href="{{ route("folders.show", ["username" => $folder->user()->first()->name, "folder"=>$folder]) }}">
+						{{ $folder->title }}</a><!--
+					-->@if(!$loop->last), @endif
+				@endforeach
+			</div>
+			@endif
+
 			@if($text)
 			<div class="artwork-text">Description: {!! $text !!}</div>
 			@endif
