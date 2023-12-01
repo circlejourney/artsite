707adc29bc7e19228264edfbe069034880ce9c24
Amari Low <sceptile_rayquaza@hotmail.com>
2023-12-01 16:30:45 +1000

    Add meta to global tag search page

--- resources/views/tags/show-global.blade.php
+++ resources/views/tags/show-global.blade.php
@@ -1,8 +1,12 @@
-@extends("layouts.site")
+@extends("layouts.site", ["metatitle" => "Artwork tagged #".$tagName])
+
+@push('head')
+	<meta name="og:image" content="{{ $artworks->first()->getThumbnailURL() }}">
+@endpush
 
 @section('body')
 	<div class="page-block">
-		<h1>Global feed for tag: {{ $tagName}}</h1>
+		<h1>Artwork tagged #{{$tagName}}</h1>
 		@include("layouts.gallery", ["artworks" => $artworks])
 	</div>
 @endsection
