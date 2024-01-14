113b7807288d4ea2d97ff90c6bccf8170d2b7ed1
Amari Low <sceptile_rayquaza@hotmail.com>
2024-01-14 17:36:50 +1000

    Fix typo making tag thumbnail not show

--- resources/views/components/tag-highlight.blade.php
+++ resources/views/components/tag-highlight.blade.php
@@ -1,6 +1,6 @@
 
 <div class="tag-highlight">
-	@if($tag->tag_highlight_thumbnail)
+	@if($tag->tag_highlight->thumbnail)
 		<img class="tag-highlight-image" src="{{ $tag->tag_highlight->getThumbnailURL() }}">
 	@endif
 	<div class="tag-highlight-text">
