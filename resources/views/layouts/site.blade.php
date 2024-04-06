b99413e1727fd99f6f7e115314d998dee0572cb3
circlejourney <sceptile_rayquaza@hotmail.com>
2024-04-06 14:33:02 +1000

    View cleanup

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -13,6 +13,7 @@
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="/src/bootstrap/bootstrap.min.css">
     <script src="/src/bootstrap/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/src/bootstrap-fix.css">
 
     <!-- FONT AWESOME -->
     <script src="https://kit.fontawesome.com/77ce6977ef.js" crossorigin="anonymous"></script>
