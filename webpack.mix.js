1e28364aea5aa6152e06e9d05c50f7a79fd8d361
circlejourney <sceptile_rayquaza@hotmail.com>
2024-04-06 14:30:33 +1000

    Reorganise styling

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 
 mix.sass('resources/src/site.scss', 'public/src')
-	.sass('resources/src/ace.scss', 'public/src');
+	.sass('resources/src/ace.scss', 'public/src')
+	.sass('resources/src/bootstrap-fix.scss', 'public/src');
 mix.browserSync('localhost:8000');
